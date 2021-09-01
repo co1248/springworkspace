48c5961a7275ea5f6e34f62df466a08154b12e59
co1248 <83028206+co1248@users.noreply.github.com>
2021-09-01 14:29:58 +0900

    Add files via upload
    0901

--- BoardWeb3-1/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb3-1/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -3,6 +3,7 @@ package com.springbook.view.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,9 +16,22 @@ import com.springbook.biz.board.impl.BoardDAO;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
+import oracle.net.aso.e;
+
 @WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HandlerMapping handlerMapping;
+	private ViewResolver viewResolver;
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		handlerMapping = new HandlerMapping();
+		viewResolver = new ViewResolver();
+		viewResolver.setPrefix("./");
+		viewResolver.setSuffix(".jsp");
+	}
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -36,87 +50,100 @@ public class DispatcherServlet extends HttpServlet {
 		
 		System.out.println(path);
 		
-		if(path.equals("/login.do")) {
-			System.out.println("∑Œ±◊¿Œ√≥∏Æ");
-			
-			String id = request.getParameter("id");
-			String password = request.getParameter("password");
-			
-			UserVO vo = new UserVO();
-			vo.setId(id);
-			vo.setPassword(password);
-			UserDAO userDAO = new UserDAO();
-			UserVO user = userDAO.getUser(vo);
-			
-			if(user != null) {
-				response.sendRedirect("getBoardList.do");
-			} else {
-				response.sendRedirect("login.jsp");
-			}
-			
-		} else if(path.equals("/getBoardList.do")) {
-			System.out.println("±€ ∏Ò∑œ ∞Àªˆ √≥∏Æ");
-			
-			BoardVO vo = new BoardVO();
-			BoardDAO boardDAO = new BoardDAO();
-			List<BoardVO> boardList = boardDAO.getBoardList(vo);
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("boardList", boardList);
-			response.sendRedirect("getBoardList.jsp");
-			
-		}  else if(path.equals("/getBoard.do")) {
-			System.out.println("±€ ªÛºº ¡∂»∏ √≥∏Æ");
-			String seq = request.getParameter("seq");
-			
-			BoardVO vo = new BoardVO();
-			vo.setSeq(Integer.parseInt(seq));
-			BoardDAO boardDAO = new BoardDAO();
-			BoardVO board = boardDAO.getBoard(vo);
-			
-			HttpSession session = request.getSession();
-			session.setAttribute("board", board);
-			response.sendRedirect("getBoard.jsp");
-		}  else if(path.equals("/insertBoard.do")) {
-			System.out.println("±€ µÓ∑œ √≥∏Æ");
-			
-			String title = request.getParameter("title");
-			String writer = request.getParameter("writer");
-			String content = request.getParameter("content");
-			BoardVO vo = new BoardVO();
-			vo.setTitle(title);
-			vo.setWriter(writer);
-			vo.setContent(content);
-			BoardDAO boardDAO = new BoardDAO();
-			boardDAO.insertBoard(vo);
-			response.sendRedirect("getBoardList.do");
-		}  else if(path.equals("/updateBoard.do")) {
-			System.out.println("±€ ºˆ¡§ √≥∏Æ");
-			
-			String seq = request.getParameter("seq");
-		    String title = request.getParameter("title");
-			String content = request.getParameter("content");
-			BoardVO vo = new BoardVO();
-			vo.setTitle(title);
-			vo.setContent(content);
-			vo.setSeq(Integer.parseInt(seq));
-			BoardDAO boardDAO = new BoardDAO();
-			boardDAO.updateBoard(vo);
-			response.sendRedirect("getBoardList.do");
-		}  else if(path.equals("/deleteBoard.do")) {
-			System.out.println("±€ ªË¡¶ √≥∏Æ");
-			
-			String seq = request.getParameter("seq");
-			BoardVO vo = new BoardVO();
-			vo.setSeq(Integer.parseInt(seq));
-			BoardDAO boardDAO = new BoardDAO();
-			boardDAO.deleteBoard(vo);
-			response.sendRedirect("getBoardList.do");
-		}  else if(path.equals("/logout.do")) {
-			System.out.println("∑Œ±◊æ∆øÙ √≥∏Æ");
-			HttpSession session = request.getSession();
-		    session.invalidate();
-		    response.sendRedirect("login.jsp");
+		Controller ctrl = handlerMapping.getController(path);
+		String viewName = ctrl.handleRequest(request, response);
+		
+		String view = null;
+		if(!viewName.contains(".do")) {
+			view = viewResolver.getView(viewName);
+		} else {
+			view = viewName;
 		}
+		
+		response.sendRedirect(view);
+		
+		
+//		if(path.equals("/login.do")) {
+//			System.out.println("Î°úÍ∑∏Ïù∏Ï≤òÎ¶¨");
+//			
+//			String id = request.getParameter("id");
+//			String password = request.getParameter("password");
+//			
+//			UserVO vo = new UserVO();
+//			vo.setId(id);
+//			vo.setPassword(password);
+//			UserDAO userDAO = new UserDAO();
+//			UserVO user = userDAO.getUser(vo);
+//			
+//			if(user != null) {
+//				response.sendRedirect("getBoardList.do");
+//			} else {
+//				response.sendRedirect("login.jsp");
+//			}
+//			
+//		} else if(path.equals("/getBoardList.do")) {
+//			System.out.println("Í∏Ä Î™©Î°ù Í≤ÄÏÉâ Ï≤òÎ¶¨");
+//			
+//			BoardVO vo = new BoardVO();
+//			BoardDAO boardDAO = new BoardDAO();
+//			List<BoardVO> boardList = boardDAO.getBoardList(vo);
+//			
+//			HttpSession session = request.getSession();
+//			session.setAttribute("boardList", boardList);
+//			response.sendRedirect("getBoardList.jsp");
+//			
+//		}  else if(path.equals("/getBoard.do")) {
+//			System.out.println("Í∏Ä ÏÉÅÏÑ∏ Ï°∞Ìöå Ï≤òÎ¶¨");
+//			String seq = request.getParameter("seq");
+//			
+//			BoardVO vo = new BoardVO();
+//			vo.setSeq(Integer.parseInt(seq));
+//			BoardDAO boardDAO = new BoardDAO();
+//			BoardVO board = boardDAO.getBoard(vo);
+//			
+//			HttpSession session = request.getSession();
+//			session.setAttribute("board", board);
+//			response.sendRedirect("getBoard.jsp");
+//		}  else if(path.equals("/insertBoard.do")) {
+//			System.out.println("Í∏Ä Îì±Î°ù Ï≤òÎ¶¨");
+//			
+//			String title = request.getParameter("title");
+//			String writer = request.getParameter("writer");
+//			String content = request.getParameter("content");
+//			BoardVO vo = new BoardVO();
+//			vo.setTitle(title);
+//			vo.setWriter(writer);
+//			vo.setContent(content);
+//			BoardDAO boardDAO = new BoardDAO();
+//			boardDAO.insertBoard(vo);
+//			response.sendRedirect("getBoardList.do");
+//		}  else if(path.equals("/updateBoard.do")) {
+//			System.out.println("Í∏Ä ÏàòÏ†ï Ï≤òÎ¶¨");
+//			
+//			String seq = request.getParameter("seq");
+//		    String title = request.getParameter("title");
+//			String content = request.getParameter("content");
+//			BoardVO vo = new BoardVO();
+//			vo.setTitle(title);
+//			vo.setContent(content);
+//			vo.setSeq(Integer.parseInt(seq));
+//			BoardDAO boardDAO = new BoardDAO();
+//			boardDAO.updateBoard(vo);
+//			response.sendRedirect("getBoardList.do");
+//		}  else if(path.equals("/deleteBoard.do")) {
+//			System.out.println("Í∏Ä ÏÇ≠Ï†ú Ï≤òÎ¶¨");
+//			
+//			String seq = request.getParameter("seq");
+//			BoardVO vo = new BoardVO();
+//			vo.setSeq(Integer.parseInt(seq));
+//			BoardDAO boardDAO = new BoardDAO();
+//			boardDAO.deleteBoard(vo);
+//			response.sendRedirect("getBoardList.do");
+//		}  else if(path.equals("/logout.do")) {
+//			System.out.println("Î°úÍ∑∏ÏïÑÏõÉ Ï≤òÎ¶¨");
+//			HttpSession session = request.getSession();
+//		    session.invalidate();
+//		    response.sendRedirect("login.jsp");
+//		}
 	}
 }
