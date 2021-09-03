1db34a8ac1280a1c3efcdd76cd9ec4d30b6de1af
co1248 <83028206+co1248@users.noreply.github.com>
2021-09-03 17:25:38 +0900

    Add files via upload
    0903

---
+++ BoardWeb4/src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -0,0 +1,24 @@
+package com.springbook.view.board;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+@Controller
+public class GetBoardListController{
+
+	@RequestMapping(value="/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo , BoardDAO boardDAO, ModelAndView mav /*command object*/) {
+		// TODO Auto-generated method stub
+		System.out.println("글 목록 검색 처리");
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		mav.addObject("boardList", boardList);
+		mav.setViewName("getBoardList.jsp");
+		return mav;
+	}
+}
