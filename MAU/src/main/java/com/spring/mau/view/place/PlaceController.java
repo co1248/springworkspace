664c440b33fe43290581222ec695642059db0f7e
co1248 <83028206+co1248@users.noreply.github.com>
2021-10-22 18:29:44 +0900

    Add files via upload
    1022

--- MAU/src/main/java/com/spring/mau/view/place/PlaceController.java
+++ MAU/src/main/java/com/spring/mau/view/place/PlaceController.java
@@ -1,6 +1,7 @@
 package com.spring.mau.view.place;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.mau.mapplace.MapPlaceService;
 import com.spring.mau.mapplace.MapPlaceVO;
+import com.spring.mau.mapview.MapViewService;
+import com.spring.mau.mapview.MapViewVO;
 import com.spring.mau.place.PlaceService;
 import com.spring.mau.place.PlaceVO;
 import com.spring.mau.userplace.UserPlaceService;
@@ -31,6 +34,8 @@ public class PlaceController {
 	private UserPlaceService userPlaceService;
 	@Autowired
 	private MapPlaceService mapPlaceService;
+	@Autowired
+	private MapViewService mapViewService;
 	
 	@RequestMapping(value="/guideMap/{mapSeq}", method = RequestMethod.POST)
 	public void insertPlace (@PathVariable("mapSeq") int mapSeq, PlaceVO vo, UserPlaceVO vo2, MapPlaceVO vo3, HttpServletRequest request, HttpServletResponse response) {
@@ -46,8 +51,14 @@ public class PlaceController {
 		vo.setPlaceNorthEast(placeNorthEast);
 		vo.setPlaceName(placeName);
 		vo.setPlaceAddr(placeAddr);
-		placeService.insertPlace(vo);
-		PlaceVO plSeq = placeService.getPlaceSeq(vo);//새로 만들어진 장소의 시퀀스 값(placeSeq) 가져오기
+		PlaceVO plId = placeService.getPlaceId(vo);
+		PlaceVO plSeq;
+		if(plId == null) {//새로운 장소등록
+			placeService.insertPlace(vo);
+			plSeq = placeService.getPlaceSeq(vo);//새로 만들어진 장소의 시퀀스 값(placeSeq) 가져오기
+		} else {//이미있는 장소등록은 insertPlace하지 않는다.
+			plSeq = placeService.getPlaceId(vo);
+		}
 		System.out.println("유저-장소등록");
 		vo2.setPlaceSeq(plSeq.getPlaceSeq());
 		vo2.setMapSeq(mapSeq);
@@ -69,12 +80,33 @@ public class PlaceController {
 	}
 	
 	@RequestMapping(value="/guideMap/search/{mapSeq}", method = RequestMethod.POST)
-	public ModelAndView searchPlace (@PathVariable("mapSeq") int mapSeq, PlaceVO vo,HttpServletRequest request, Model model) {
+	public ModelAndView searchPlace (@PathVariable("mapSeq") int mapSeq, PlaceVO vo,MapViewVO vo2,HttpServletRequest request, Model model) {
 		// TODO Auto-generated method stub
 		System.out.println("장소검색");
 		String keyword = request.getParameter("keyword");
 		request.setAttribute("keyword", keyword);
+		
+		vo2.setMapSeq(mapSeq);
+		MapViewVO map = mapViewService.getBySeq(vo2);
+		model.addAttribute("map", map);
+		
 		return new ModelAndView("/guide/searchplace.jsp");
 	}
+	
+	@RequestMapping("myfavorite")
+	public ModelAndView getFavoritePlace (PlaceVO vo,MapViewVO vo2, Model model, HttpSession session, HttpServletRequest request ) {
+		// TODO Auto-generated method stub
+		System.out.println("장소즐겨찾기(지도형태로) 상세 보기");
+		session = request.getSession();
+		int userSeqId = (Integer)session.getAttribute("userSeqId");
+		System.out.println(userSeqId);
+		/*
+		 * vo2.setUserSeqId(userSeqId); List<MapViewVO> placeList =
+		 * placeService.getfavoritePlaceSeq(vo2); model.addAttribute("placeList",
+		 * placeList);
+		 */
+		return new ModelAndView("../myavorite.jsp");
+	}
+
 	
 }
